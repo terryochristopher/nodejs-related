6b0de15d87157a718312e42300c8eadcadf99f24
terryochristopher <terryochristopher@gmail.com>
2023-11-24 11:07:32 -0500

    changes made

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+// express app
+const app = express();
+
+// listen to express 
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    res.sendFile('./views/index.html', {root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', {root: __dirname });
+});
+
+// redirect
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+});
+
+//404 page
+app.use((req, res) =>{
+    res.sendFile('./views/404.html', {root: __dirname});
+
+});
