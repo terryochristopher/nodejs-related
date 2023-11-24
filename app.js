63eb510648b6a5ee977f5b4ae12982be90172b1a
terryochristopher <terryochristopher@gmail.com>
2023-11-24 11:22:00 -0500

    using ejs

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const express = require('express');
 // express app
 const app = express();
 
+
+// Register view engine
+app.set('view engine', 'ejs');
+
+
 // listen to express 
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname });
+    // res.sendFile('./views/index.html', {root: __dirname });
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
